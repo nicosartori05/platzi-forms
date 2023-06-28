761104d9b0ae49d4e0335f7b2bc80597bf2c127e
nicosartori05 <nicolas.sartori@davinci.edu.ar>
2023-06-28 14:28:46 -0300

    comit

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl,Validators } from '@angular/forms';
+
+
 @Component({
   selector: 'app-basic-form',
   templateUrl: './basic-form.component.html',
@@ -7,7 +9,7 @@ import { FormControl } from '@angular/forms';
 })
 export class BasicFormComponent implements OnInit {
 
-  nameField = new FormControl('');
+  nameField = new FormControl('', [Validators.required, Validators.maxLength(10)]);
   emailField = new FormControl('');
   phoneField = new FormControl('');
   colorField = new FormControl('#000000');
