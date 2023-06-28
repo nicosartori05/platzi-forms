19946a061191641a7d03aadd56054264f7916dba
nicosartori05 <nicolas.sartori@davinci.edu.ar>
2023-06-28 11:43:41 -0300

    first commit

---
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+@Component({
+  selector: 'app-basic-form',
+  templateUrl: './basic-form.component.html',
+  styleUrls: ['./basic-form.component.scss']
+})
+export class BasicFormComponent implements OnInit {
+
+  nameField = new FormControl('soy un control');
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.nameField.valueChanges
+    .subscribe(value => {
+      console.log(value)
+    });
+  }
+
+  getNameValue(){
+    console.log(this.nameField.value);
+  }
+}
